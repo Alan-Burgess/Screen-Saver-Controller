0ffd3b306159ce8b653e21e9754f1886c735085e
Alan <alan@brock-house.com>
2024-10-05 12:15:22 +0100

    Add project files.

---
+++ Screen Saver Controller/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Screen_Saver_Controller
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Screen Saver Controller/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Screen_Saver_Controller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [ObservableObject]
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            IsScreenSaverActive = true;
+        }
+
+        [ObservableProperty]
+        private string _buttonText = "Disable Screen Saver";
+
+        [ObservableProperty]
+        private bool _isScreenSaverActive;
+
+        partial void OnIsScreenSaverActiveChanged(bool value)
+        {
+            if (value)
+            {
+                PreventScreenSaver.IsActive = true;
+                ButtonText = "Screen Saver Disabled";
+            }
+            else
+            {
+                PreventScreenSaver.IsActive = false;
+                ButtonText = "Disable Screen Saver";
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            PreventScreenSaver.IsActive = false;
+        }
+    }
+}
